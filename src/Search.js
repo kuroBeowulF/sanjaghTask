c63138ebe4311456347621170f5a3d5505665fd6
k.fazli <KuroBeowulF@gmail.com>
2023-06-23 16:03:33 +0430

    sanjagh

--- src/Search.js
+++ src/Search.js
@@ -9,11 +9,11 @@ const FeaturefulSearch = () => {
 
   return (
     <>
-      <h1 className="sm:hidden m-4 mt-10 text-center font-bold">
+      <h1 className="m-4 mt-10 text-center font-bold">
         میتوانید خدمات مختلف را در شهر مورد نظر خود جستجو کنید
       </h1>
       <FullPageSearch
-        containerClassName="flex sm:hidden w-full px-3 py-4 overflow-hidden"
+        containerClassName="flex max-w-xl px-3 py-4 overflow-hidden"
         // enabledZones={[]}
         zoneName={currentZone.zoneName}
         zoneId={currentZone.zoneId}
